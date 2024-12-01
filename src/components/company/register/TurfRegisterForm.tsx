16ef81697e5bb07114f5ae0cad456219ba529b89
YaseerM16 <yaseerbscit@gmail.com>
2024-12-01 12:08:20 +0530

    Turf Register form Validation Done.

--- src/components/company/register/TurfRegisterForm.tsx
+++ src/components/company/register/TurfRegisterForm.tsx
@@ -4,9 +4,14 @@ import React from "react";
 import { useForm, Controller } from "react-hook-form";
 import "react-calendar/dist/Calendar.css";
 import dynamic from "next/dynamic";
+import "react-toastify/dist/ReactToastify.css";
+
 
 const Calendar = dynamic(() => import("react-calendar"), { ssr: false });
-
+interface Marker {
+    latitude: number;
+    longitude: number;
+}
 interface TurfRegisterFormData {
     turfName: string;
     description: string;
@@ -18,34 +23,38 @@ interface TurfRegisterFormData {
     fromTime: string;
     toTime: string;
     selectedFacilities: string[];
+    location: Marker | null;
 }
 
 interface TurfRegisterFormProps {
     onSubmit: (data: TurfRegisterFormData) => void;
-    handleLocationRequest: () => void; // Location request function passed as prop
-
+    handleLocationRequest: () => void;
+    MapValidate: Marker | null;
 }
 
-const TurfRegisterForm: React.FC<TurfRegisterFormProps> = ({ onSubmit, handleLocationRequest }) => {
+const TurfRegisterForm: React.FC<TurfRegisterFormProps> = ({ onSubmit, handleLocationRequest, MapValidate }) => {
     const {
         register,
         handleSubmit,
         control,
         setValue,
+        setError,
+        clearErrors,
         watch,
-        formState: { errors },
+        formState: { errors }
     } = useForm<TurfRegisterFormData>({
         defaultValues: {
             turfName: "",
             description: "",
-            turfSize: "5s",
-            turfType: "Open",
+            turfSize: "",
+            turfType: "",
             price: "",
             images: [],
             workingDays: [],
             fromTime: "",
             toTime: "",
             selectedFacilities: [],
+            location: null
         },
     });
 
@@ -57,6 +66,7 @@ const TurfRegisterForm: React.FC<TurfRegisterFormProps> = ({ onSubmit, handleLoc
             ? selectedFacilities.filter((f) => f !== facility)
             : [...selectedFacilities, facility];
 
+        clearErrors("selectedFacilities")
         setValue("selectedFacilities", updatedFacilities);
     };
 
@@ -65,21 +75,50 @@ const TurfRegisterForm: React.FC<TurfRegisterFormProps> = ({ onSubmit, handleLoc
             ? workingDays.filter((d) => d !== day)
             : [...workingDays, day];
 
+        clearErrors("workingDays")
         setValue("workingDays", updatedDays);
     };
 
     const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.files) {
-            setValue("images", Array.from(event.target.files));
+            clearErrors("images")
+            const files = Array.from(event.target.files);
+            setValue("images", files); // Update the form state manually
         }
     };
 
-    const handleTurfSizeSelection = (size: string) => {
-        setValue("turfSize", size);
+
+    const handleFormSubmit = (data: any) => {
+        try {
+            // Check if no images are selected
+            console.log("MapValidate val:", MapValidate);
+
+            if (!data.images || data.images.length === 0) {
+                setError("images", { type: "manual", message: "Please upload at least one turf image" });
+            } else if (!data.workingDays || data.workingDays.length === 0) {
+                setError("workingDays", { type: "manual", message: "Select Atleast One working day !" })
+            } else if (!data.selectedFacilities || data.selectedFacilities.length === 0) {
+                setError("selectedFacilities", { type: "manual", message: "Select Atleast One Facility !" })
+            }
+            //  else if (!MapValidate) {
+            //     setError("location", { type: "manual", message: "Provide Turf Location !" })
+
+            // }
+            else {
+                if (!MapValidate) {
+                    setError("location", { type: "manual", message: "Provide Turf Location !" })
+                    return
+                }
+                clearErrors("location")
+                onSubmit(data); // If images are selected, submit the form
+            }
+        } catch (error) {
+            console.log("Error While Submitting from the Turf Form :", error);
+        }
     };
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+        <form onSubmit={handleSubmit(handleFormSubmit)} className="grid grid-cols-1 lg:grid-cols-3 gap-6">
             {/* Left Column */}
             <div className="space-y-6">
                 <div className="p-4 rounded-lg shadow-lg bg-white">
@@ -102,33 +141,40 @@ const TurfRegisterForm: React.FC<TurfRegisterFormProps> = ({ onSubmit, handleLoc
                         {errors.description && <p className="text-red-500 text-sm">{errors.description.message}</p>}
                     </div>
                     <div className="mb-4">
-                        <label className="block text-sm font-semibold text-green-900 mb-2">Size</label>
-                        <div className="flex gap-2">
-                            {["5s", "7s", "11s"].map((option) => (
-                                <button
-                                    type="button"
-                                    key={option}
-                                    className={`px-4 py-2 rounded-lg font-bold text-sm ${watch("turfSize") === option ? "bg-green-600 text-white" : "bg-gray-300 text-green-800"
-                                        }`}
-                                    onClick={() => handleTurfSizeSelection(option)}
-                                >
-                                    {option}
-                                </button>
-                            ))}
-                        </div>
+                        <label htmlFor="turfSize" className="block text-sm font-semibold text-green-900 mb-2">
+                            Turf Size
+                        </label>
+                        <select
+                            id="turfSize"
+                            className="w-full px-4 py-2 border rounded-lg focus:ring focus:ring-green-400"
+                            {...register("turfSize", { required: "Please select the turf size" })}
+                        >
+                            <option value="">Select Size</option>
+                            <option value="5s">5s</option>
+                            <option value="7s">7s</option>
+                            <option value="11s">11s</option>
+                        </select>
+                        {errors.turfSize && <p className="text-red-500 text-sm">"Please select the turf size"{errors.turfSize.message}</p>}
                     </div>
+
 
 
                     <div className="mb-4">
-                        <label className="block text-sm font-semibold text-green-900 mb-2">Turf Type</label>
+                        <label htmlFor="turfType" className="block text-sm font-semibold text-green-900 mb-2">
+                            Turf Type
+                        </label>
                         <select
+                            id="turfType"
                             className="w-full px-4 py-2 border rounded-lg focus:ring focus:ring-green-400"
-                            {...register("turfType", { required: "Turf type is required" })}
+                            {...register("turfType", { required: "Please select the turf type" })}
                         >
+                            <option value="">Select Turf Type</option>
                             <option value="Open">Open</option>
                             <option value="Closed">Closed</option>
                         </select>
+                        {errors.turfType && <p className="text-red-500 text-sm">{errors.turfType.message}</p>}
                     </div>
+
                     <div className="mb-4">
                         <label className="block text-sm font-semibold text-green-900 mb-2">Price</label>
                         <input
@@ -146,15 +192,18 @@ const TurfRegisterForm: React.FC<TurfRegisterFormProps> = ({ onSubmit, handleLoc
                 <div className="p-4 rounded-lg shadow-lg bg-white">
                     <h2 className="text-xl font-semibold text-green-800 mb-4">Facilities</h2>
                     <div className="mb-4">
-                        <label className="block text-sm font-semibold text-green-900 mb-2">Turf Images</label>
+                        <label htmlFor="turfImages" className="block text-sm font-semibold text-green-900 mb-2">Turf Images</label>
                         <input
+                            id="turfImages"
                             type="file"
                             multiple
                             className="w-full px-4 py-2 border rounded-lg focus:ring focus:ring-green-400"
                             onChange={handleImageUpload}
                         />
+                        {errors.images && <p className="text-red-500 text-lg mt-2">{errors.images.message}</p>}
                     </div>
                 </div>
+
                 <div className="p-4 rounded-lg shadow-lg bg-white">
                     <h2 className="text-xl font-semibold text-green-800 mb-4">Facilities</h2>
                     <div className="flex flex-wrap gap-4">
@@ -169,6 +218,8 @@ const TurfRegisterForm: React.FC<TurfRegisterFormProps> = ({ onSubmit, handleLoc
                             </button>
                         ))}
                     </div>
+                    {errors.selectedFacilities && <p className="text-red-500 text-lg mt-2">{errors.selectedFacilities.message}</p>}
+
                 </div>
 
             </div>
@@ -185,6 +236,7 @@ const TurfRegisterForm: React.FC<TurfRegisterFormProps> = ({ onSubmit, handleLoc
                                 className="w-full px-4 py-2 border rounded-lg focus:ring focus:ring-green-400"
                                 {...register("fromTime", { required: "From time is required" })}
                             />
+                            {errors.fromTime && <p className="text-red-500 text-sm">{errors.fromTime.message}</p>}
                         </div>
                         <div className="w-1/2">
                             <label className="block text-sm font-semibold text-green-900 mb-2">To</label>
@@ -193,7 +245,9 @@ const TurfRegisterForm: React.FC<TurfRegisterFormProps> = ({ onSubmit, handleLoc
                                 className="w-full px-4 py-2 border rounded-lg focus:ring focus:ring-green-400"
                                 {...register("toTime", { required: "To time is required" })}
                             />
+                            {errors.toTime && <p className="text-red-500 text-sm">{errors.toTime.message}</p>}
                         </div>
+
                     </div>
                     <div className="mb-4">
                         <label className="block text-sm font-semibold text-green-900 mb-2">Working Days</label>
@@ -209,6 +263,8 @@ const TurfRegisterForm: React.FC<TurfRegisterFormProps> = ({ onSubmit, handleLoc
                                 </button>
                             ))}
                         </div>
+                        {errors.workingDays && <p className="text-red-500 text-lg mt-2">{errors.workingDays.message}</p>}
+
                     </div>
                 </div>
                 <div className="flex flex-col space-y-6">
@@ -221,6 +277,7 @@ const TurfRegisterForm: React.FC<TurfRegisterFormProps> = ({ onSubmit, handleLoc
                         >
                             Select Location
                         </button>
+                        {errors.location && <p className="text-red-500 text-lg mt-2">{errors.location.message}</p>}
                     </div>
                 </div>
             </div>
