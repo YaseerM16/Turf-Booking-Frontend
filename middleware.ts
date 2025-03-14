e364ddc87a805b942ed3b29a8520ff54e87fa348
YaseerM16 <yaseerbscit@gmail.com>
2025-03-14 09:19:10 +0530

    deleted next cache

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import { userMiddleware } from "./src/middleware/userMiddleware";
 import { companyMiddleware } from "./src/middleware/companyMiddleware";
 import { adminMiddleware } from "./src/middleware/adminMiddleware";
 console.error("HIIIII  FRoommmyy fht YYYSSSSRRRRRRR ");
+console.error("HIIIII  FRoommmyy fht YYYSSSSRRRRRRR ");
 
 
 export function middleware(req: NextRequest) {
