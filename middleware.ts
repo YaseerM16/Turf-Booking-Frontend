967dd2526993ea5b38c66d89d79524cefdd671a7
YaseerM16 <yaseerbscit@gmail.com>
2025-03-15 09:55:00 +0530

    return bac the Middleware file

--- middleware.ts
+++ middleware.ts
@@ -2,30 +2,28 @@ import { NextRequest, NextResponse } from "next/server";
 import { userMiddleware } from "./src/middleware/userMiddleware";
 import { companyMiddleware } from "./src/middleware/companyMiddleware";
 import { adminMiddleware } from "./src/middleware/adminMiddleware";
-console.log("HIIIII  FRoommmyy fht YYYSSSSRRRRRRR ");
-
 
 export function middleware(req: NextRequest) {
-    // const currentPath = req.nextUrl.pathname;
+    const currentPath = req.nextUrl.pathname;
 
-    // if (currentPath.startsWith("/admin")) {
-    //     return adminMiddleware(req);
-    // }
-    // if (currentPath.startsWith("/company")) {
-    //     return companyMiddleware(req);
-    // }
-    // if (currentPath.startsWith("/profile") ||
-    //     currentPath.startsWith("/my-bookings") ||
-    //     currentPath.startsWith("/my-wallet") ||
-    //     currentPath.startsWith("/messages") ||
-    //     currentPath.startsWith("/bookingSuccess") ||
-    //     currentPath.startsWith("/login") ||
-    //     currentPath.startsWith("/signup") ||
-    //     currentPath.startsWith("/verifymail") ||
-    //     currentPath.startsWith("/checkmail") ||
-    //     currentPath.startsWith("/forgotpassword")) {
-    //     return userMiddleware(req);
-    // }
+    if (currentPath.startsWith("/admin")) {
+        return adminMiddleware(req);
+    }
+    if (currentPath.startsWith("/company")) {
+        return companyMiddleware(req);
+    }
+    if (currentPath.startsWith("/profile") ||
+        currentPath.startsWith("/my-bookings") ||
+        currentPath.startsWith("/my-wallet") ||
+        currentPath.startsWith("/messages") ||
+        currentPath.startsWith("/bookingSuccess") ||
+        currentPath.startsWith("/login") ||
+        currentPath.startsWith("/signup") ||
+        currentPath.startsWith("/verifymail") ||
+        currentPath.startsWith("/checkmail") ||
+        currentPath.startsWith("/forgotpassword")) {
+        return userMiddleware(req);
+    }
 
     return NextResponse.next();
 }
